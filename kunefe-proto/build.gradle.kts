416c91758ac02c5a6be869813cfc55663bcc4bd3
selimsahindev <selimsahin.sns@gmail.com>
2026-06-20 22:42:29 +0300

    feat(proto): define broker, producer, consumer service contracts

---
+++ kunefe-proto/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.google.protobuf") version "0.9.4"
+}
+
+dependencies {
+    implementation("io.grpc:grpc-protobuf:1.64.0")
+    implementation("io.grpc:grpc-stub:1.64.0")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.3"
+    }
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.64.0"
+        }
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                create("grpc")
+            }
+        }
+    }
+}
